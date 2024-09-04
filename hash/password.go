1e743ab6b271c6db275e0c0a0f77392b5ba5e3c9
Madina <jumaniyozovamadina71@gmail.com>
2024-09-04 10:44:34 +0500

    added new function

--- hash/password.go
+++ hash/password.go
@@ -15,3 +15,7 @@ func CheckPassword(hashedPassword, password string)bool{
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err==nil
 }
+
+func AddInteger(a, b int)int{
+	return a+b
+}
